f3841aaa1152fdd37eb4a53cf622bed689801c6d
Kalab21 <kkebed21@gmail.com>
2022-06-04 18:22:14 -0500

    add All

---
+++ practice/practic.js
@@ -0,0 +1,28 @@
+// 
+// let m = document.getElementById('result');
+// m.innerText = max(10, 15);
+// var x = 10;
+
+// let y = 10;
+
+// y++; {
+//     y++
+//     // console.log(x)
+// }
+// console.log(y);
+// console.log(text)
+// var text="getu"
+//     // console.log(sum());
+// console.log(product(2, 3));
+// let sum = (a, b) => (a + b);
+// const product = (a, b) => a * b;
+
+console.log(cal(10, 30));
+
+function cal(a, b) {
+    return a + b;
+}
+
+
+
+var xy = 10;
